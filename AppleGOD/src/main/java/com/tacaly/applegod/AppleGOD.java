ac811030950d00fa83ec6977a7e9b2411fd747d4
Frederick Nicklas Ambo Eggert Eggertsen <lederafbk@gmail.com>
2020-10-11 01:27:15 +0200

    0.0.6 release
    Updated code more clean.
    Added enchanted apple

--- AppleGOD/src/main/java/com/tacaly/applegod/AppleGOD.java
+++ AppleGOD/src/main/java/com/tacaly/applegod/AppleGOD.java
@@ -1,8 +1,5 @@
 package com.tacaly.applegod;
 
-import org.bukkit.Material;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AppleGOD extends JavaPlugin {
@@ -17,39 +14,8 @@ public final class AppleGOD extends JavaPlugin {
         //command name set
         getCommand("applegodv").setExecutor(new command());
 
-
-        //Normal Apple recipe
-
-        ItemStack applegnormal = new ItemStack(Material.APPLE, 1);
-
-        @SuppressWarnings("deprecation")
-        ShapedRecipe APPLEYOUNG = new ShapedRecipe(applegnormal);
-
-        //crafting grid
-        APPLEYOUNG.shape("*%*","%B%","*%*");
-
-        APPLEYOUNG.setIngredient('*', Material.STICK);
-        APPLEYOUNG.setIngredient('%', Material.COBBLESTONE);
-        APPLEYOUNG.setIngredient('B', Material.WHEAT_SEEDS);
-
-        // Add recipe to server
-        getServer().addRecipe(APPLEYOUNG);
-
-        //golden apple recipe
-        ItemStack applegGoldem = new ItemStack(Material.GOLDEN_APPLE, 1);
-
-        @SuppressWarnings("deprecation")
-        ShapedRecipe APPLEGOLDEN = new ShapedRecipe(applegGoldem);
-
-        //crafting grid
-        APPLEGOLDEN.shape("*%*","%B%","*%*");
-
-        APPLEGOLDEN.setIngredient('*', Material.STICK);
-        APPLEGOLDEN.setIngredient('%', Material.GOLD_INGOT);
-        APPLEGOLDEN.setIngredient('B', Material.APPLE);
-
-        // Add recipe to server of golden apple
-        getServer().addRecipe(APPLEGOLDEN);
+        // call java class recipes
+        recipes.recipe();
     }
 
     @Override
--- AppleGOD/src/main/java/com/tacaly/applegod/recipes.java
+++ AppleGOD/src/main/java/com/tacaly/applegod/recipes.java
@@ -4,15 +4,18 @@ import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 
+import static org.bukkit.Bukkit.getServer;
+
 
 public class recipes {
-    public void recipesvoid() {
+    public static void recipe() {
+        //@SuppressWarnings("deprecation")
 
         //Normal Apple recipe
         ItemStack applegnormal = new ItemStack(Material.APPLE, 1);
 
-        @SuppressWarnings("deprecation")
-        ShapedRecipe APPLEYOUNG = new ShapedRecipe(applegnormal);
+        ShapedRecipe APPLEYOUNG;
+        APPLEYOUNG = new ShapedRecipe(applegnormal);
 
         //crafting grid
         APPLEYOUNG.shape("*%*","%B%","*%*");
@@ -21,18 +24,44 @@ public class recipes {
         APPLEYOUNG.setIngredient('%', Material.COBBLESTONE);
         APPLEYOUNG.setIngredient('B', Material.WHEAT_SEEDS);
 
+        // Add recipe to server of golden apple
+        getServer().addRecipe(APPLEYOUNG);
+
+        ///////////////////////////////////////////////
         //golden apple recipe
         ItemStack applegGoldem = new ItemStack(Material.GOLDEN_APPLE, 1);
 
-        @SuppressWarnings("deprecation")
-        ShapedRecipe APPLEGOLDEN = new ShapedRecipe(applegGoldem);
+        ShapedRecipe APPLEGOLDEN;
+        APPLEGOLDEN = new ShapedRecipe(applegGoldem);
 
-        //crafting grid
+        //crafting shape
         APPLEGOLDEN.shape("*%*","%B%","*%*");
-
+        //crafting grid
         APPLEGOLDEN.setIngredient('*', Material.STICK);
         APPLEGOLDEN.setIngredient('%', Material.GOLD_INGOT);
         APPLEGOLDEN.setIngredient('B', Material.APPLE);
 
+        // Add recipe to server of golden apple
+        getServer().addRecipe(APPLEGOLDEN);
+
+        ///////////////////////////////////////////////
+        //enchanted apple
+        ItemStack appleEnchanted = new ItemStack(Material.ENCHANTED_GOLDEN_APPLE, 1);
+
+
+        ShapedRecipe APPLEENCHANTED;
+        APPLEENCHANTED = new ShapedRecipe(appleEnchanted);
+
+        //crafting shape
+        APPLEENCHANTED.shape("*%*","%B%","*%*");
+        //crafting grid
+        APPLEENCHANTED.setIngredient('*', Material.NETHERITE_INGOT);
+        APPLEENCHANTED.setIngredient('%', Material.EXPERIENCE_BOTTLE);
+        APPLEENCHANTED.setIngredient('B', Material.GOLDEN_APPLE);
+
+        // Add recipe to server of golden apple
+        getServer().addRecipe(APPLEENCHANTED);
+
     }
+
 }
